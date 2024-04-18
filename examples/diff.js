e09ff3d23496ba41116a5a98adc220f46beb6a39
magdaarques <magdaarques@gmail.com>
2024-04-18 11:00:29 +0200

    Create diff.js

---
+++ examples/diff.js
@@ -0,0 +1,10 @@
+//color+feedback+modulate+diff
+//@magdaarques
+speed=.8
+var v0 = ()=>Math.abs(time)
+var v1 = ()=>Math.sin(time)
+osc(10,.1,4).out(o0)
+src(o0).saturate(v1).out(o1)
+osc(10,.2,8).layer(o1).out(o2)
+src(o0).modulateScale(o2,1.1,.05).diff(o2,v0).out(o3)
+render()
