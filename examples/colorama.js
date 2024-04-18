e96d19ca1cd6bbcfb6314eb5beefb9a54ed9ec54
magdaarques <magdaarques@gmail.com>
2024-04-18 23:28:44 +0200

    Create colorama.js

---
+++ examples/colorama.js
@@ -0,0 +1,3 @@
+// Type some code on a new line (such as "osc().out()"), and press CTRL+shift+enter
+s0.initImage("https://upload.wikimedia.org/wikipedia/commons/0/0c/Stop_the_genocide%2C_Free_Palestine_023_Mielenosoitus_palestiinalaisten_tueksi_%2853274234547%29.jpg")
+src(s0).colorama(()=>Math.sin(time),()=>Math.sin(time)).out(o0)
