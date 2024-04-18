a38f4ce703beac5bcdfd76a231dbe1fdc360d554
magdaarques <magdaarques@gmail.com>
2024-04-19 00:02:24 +0200

    Create b.js

---
+++ examples/b.js
@@ -0,0 +1,6 @@
+s0.initImage("https://upload.wikimedia.org/wikipedia/commons/0/0c/Stop_the_genocide%2C_Free_Palestine_023_Mielenosoitus_palestiinalaisten_tueksi_%2853274234547%29.jpg")
+src(s0).colorama(3,.4).g().out(o0)
+src(s0).colorama(3,.4).b().out(o1)
+src(s0).colorama(3,.4).r().out(o2)
+
+render()
